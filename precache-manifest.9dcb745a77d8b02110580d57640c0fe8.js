73b3e487f2db26d3ffb06e88fc02fa5109fee12f
randilu <randilu.s@gmail.com>
2019-03-12 15:06:17 +0530

    Updates

---
+++ precache-manifest.9dcb745a77d8b02110580d57640c0fe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5a8858c39fb8b7e13b0",
+    "url": "/static/js/main.8b13e19e.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "d5a8858c39fb8b7e13b0",
+    "url": "/static/css/main.862de1b5.chunk.css"
+  },
+  {
+    "revision": "5ed81c732874767d6cfeb844ba6befa9",
+    "url": "/index.html"
+  }
+];
